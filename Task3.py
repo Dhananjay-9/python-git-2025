2c279e13f0cac3f1295284c0727d07e4ca4ea1d3
Dhananjay-9 <160595029+Dhananjay-9@users.noreply.github.com>
2025-09-24 10:56:20 +0530

    Add files via upload

---
+++ Task3.py
@@ -0,0 +1,6 @@
+Num =  int(input("Enter value to check even or odd"))
+
+if Num % 2 == 0:
+  print(f"{Num} an Even number")
+else:
+  print(f"{Num} a odd number")
---
+++ Task4.py
@@ -0,0 +1,6 @@
+Sum = 0
+
+for i in range(1,51):
+    Sum += i
+    
+print(f"Sum of this interger is {Sum} ")
